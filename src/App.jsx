42ef4b2bf3c0445d7c85b6e97eeed3e74c9a8aa5
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-17 22:17:40 -0400

    added dummy data and ObligationsList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import Header from './components/Header';
 import ObligationForm from './components/ObligationForm';
+import ObligationsList from './components/ObligationsList';
+
+const DUMMY_DATA = [
+  {
+    id: 'e1',
+    title: 'Do homework',
+    priority: 'high',
+    isActive: true,
+  },
+  {
+    id: 'e2',
+    title: 'Learn React',
+    priority: 'medium',
+    isActive: true,
+  },
+  {
+    id: 'e3',
+    title: 'Take garbage out',
+    priority: 'low',
+    isActive: true,
+  },
+  {
+    id: 'e4',
+    title: 'clean bathroom',
+    priority: 'low',
+    isActive: true,
+  },
+];
 
 function App() {
   return (
     <>
       <Header />
       <ObligationForm />
+      <ObligationsList obligations={DUMMY_DATA} />
     </>
   );
 }
