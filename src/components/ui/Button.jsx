822e90900ac768674b651ed2dde5084ca84fc75f
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-17 21:37:29 -0400

    added Button component and initial styles

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ type, text }) => {
   return (
     <button
       type={type}
-      className="py-2 px-8 rounded-xl bg-sapphire text-gray"
+      className="py-2 px-8 rounded-xl bg-sapphire text-gray drop-shadow-lg hover:bg-dark-sapphire"
     >
       {text}
     </button>
